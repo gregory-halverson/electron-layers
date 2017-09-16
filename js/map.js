ae9e8d12cdf74b77208ac7f8f794b496174b84de
gregory-halverson <gregory.halverson@gmail.com>
2017-09-16 11:50:28 -0700

    organizing map initialization

--- js/map.js
+++ js/map.js
@@ -2,16 +2,22 @@ var basemap = new ol.source.OSM({
     "url": "http://{a-c}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png"
 });
 
+var basemap_layer = new ol.layer.Tile({
+    source: basemap
+});
+
+var view = new ol.View({
+    center: ol.proj.fromLonLat([-119, 35]),
+    zoom: 6,
+    minZoom: 2
+});
+
+var layers = [
+    basemap_layer
+];
+
 var map = new ol.Map({
     target: 'map',
-    layers: [
-        new ol.layer.Tile({
-            source: basemap
-        })
-    ],
-    view: new ol.View({
-        center: ol.proj.fromLonLat([-119, 35]),
-        zoom: 6,
-        minZoom: 2
-    })
+    layers: layers,
+    view: view
 });
