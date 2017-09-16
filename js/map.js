666568443ee32fabdd41a562d45afb7f77d719a9
gregory-halverson <gregory.halverson@gmail.com>
2017-09-16 13:20:38 -0700

    can't get LayerSwitcher to work in Electron

--- js/map.js
+++ js/map.js
@@ -1,8 +1,21 @@
 var carto_dark_all = new ol.layer.Tile({
+    title: "Carto Dark",
+    type: "base",
     source: new ol.source.OSM({
         url: "http://{a-c}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png"
     })
 });
+
+var basemaps = new ol.layer.Group({
+    title: "Basemaps",
+    layers: [
+        carto_dark_all
+    ]
+});
+
+var layers = [
+    basemaps
+];
 
 var view = new ol.View({
     center: ol.proj.fromLonLat([-119, 35]),
@@ -10,12 +23,13 @@ var view = new ol.View({
     minZoom: 2
 });
 
-var layers = [
-    carto_dark_all
-];
-
 var map = new ol.Map({
     target: 'map',
     layers: layers,
     view: view
 });
+
+// var switcher = new ol.control.LayerSwitcher();
+//
+// map.addControl(switcher);
+
